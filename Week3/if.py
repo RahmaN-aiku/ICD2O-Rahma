590f94158b9c65438d14e28a25e611a23963f8ce
RahmaN-aiku <rahma@myois.org>
2024-09-20 19:15:49 +0000

    save changes

---
+++ Week3/if.py
@@ -0,0 +1,22 @@
+# if statements
+# allow us to run different code blocks on certain conditions
+a = 13
+b = 100
+if(a > b):
+    print("a is greater than b")
+if(a < b):
+    print("a is greater than b")
+    print("b is greater than a")
+#print ("b is greater than a")  # this line will print no matter what, because it is not uner a print line
+
+# excersize: declare two decimal numbers, create
+
+z = 5.8
+x = 6.7
+if(x == z):
+    print("x is greater than z")
+if(x == z):
+    print("x is greater than z")
+    print("z is greater than x")
+
+
---
+++ Week3/operators.py
@@ -0,0 +1,25 @@
+
+
+
+a = 10
+b = 5
+
+print(a+b) # addition
+print(a/b) # division
+print(a*b) # multiplying
+print(a**2) # exponent
+print(a%b) # modulus - remainder after division: print out 0
+print(8%3) # 8 / 3 and give us the remainder 
+
+# 2. assignment operators - allow us to assign values to variables 
+x = 10 #basic assignment 
+x += 5 # shorthand for x = x + 5
+
+# 3. comparison operators - allow us to compare value, and return true and false 
+m = 20
+n = 21
+
+print(m > n)
+print(m < n)
+print(m == n)
+print(m)

ae71e4b7b0d905fa304927f378c6353f7b2b3916
RahmaN-aiku <rahma@myois.org>
2024-10-18 19:22:06 +0000

    save changes

---
+++ Week7/lists.py
@@ -0,0 +1,39 @@
+#Excersize 1
+
+fruits = ['apple','banana','cherry','date']
+
+print(fruits[1])
+print(fruits[3])
+
+fruits[2] = 'orange'
+
+print(fruits)
+
+#Excersize 2
+
+numbers = ['1','2','3','4','5','6','7','8','9','10']
+
+print(numbers[0:5])
+print(numbers[7:10])
+
+#Excersize 3
+
+colors = ['red','green','blue']
+shapes = ['circle','square','triangle']
+
+print(colors + shapes)
+
+print(colors + shapes * 3)
+print(colors * 3)
+print(shapes * 3)
+
+#Excersize 4
+
+Vegetables = ['carrot', 'broccoli', 'spinach', 'potato']
+
+del Vegetables[1]
+print(Vegetables)
+print(Vegetables[0:1])
+
+#Excersize 5
+
